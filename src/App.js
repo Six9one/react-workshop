559c5a97d91255507771f8edba5651f5f24b700b
Six9one <bguirsafwan@gmail.com>
2023-09-20 17:21:02 +0200

    3aslema gomycode

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <><div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="El mail mta3ek 3ayech weldi"></input>
+    </div><div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Faregh 9albek!</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div></>
+  );
+}
+export default App;
